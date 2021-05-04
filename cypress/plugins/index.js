b59d65f7e7b6a7526bd1f8cab40b0d3a81f2ad34
John Ruble <john.ruble@atomicobject.com>
2021-05-04 15:29:17 -0400

    specify screen size

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -22,4 +22,34 @@ module.exports = (on, config) => {
   require("cypress-terminal-report/src/installLogsPrinter")(on, {
     printLogsToConsole: "always",
   });
+
+  on("before:browser:launch", (browser, launchOptions) => {
+    // https://docs.cypress.io/api/plugins/browser-launch-api#Set-screen-size-when-running-headless
+    if (browser.name === "chrome" && browser.isHeadless) {
+      // fullPage screenshot size is 1400x1200 on non-retina screens
+      // and 2800x2400 on retina screens
+      launchOptions.args.push("--window-size=1280,720");
+
+      // force screen to be non-retina (1400x1200 size)
+      launchOptions.args.push("--force-device-scale-factor=1");
+
+      // force screen to be retina (2800x2400 size)
+      // launchOptions.args.push('--force-device-scale-factor=2')
+    }
+
+    if (browser.name === "electron" && browser.isHeadless) {
+      // fullPage screenshot size is 1400x1200
+      launchOptions.preferences.width = 1280;
+      launchOptions.preferences.height = 720;
+    }
+
+    if (browser.name === "firefox" && browser.isHeadless) {
+      // menubars take up height on the screen
+      // so fullPage screenshot size is 1400x1126
+      launchOptions.args.push("--width=1280");
+      launchOptions.args.push("--height=720");
+    }
+
+    return launchOptions;
+  });
 };
